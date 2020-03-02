55595958f01ae1e35e806bc1f730905bbe26c92b
huangchong <huangchong@yeacreate.com>
2020-03-02 23:00:16 +0800

    mv yeacreate to php

--- yeacreate/restart.php
+++
@@ -1,8 +0,0 @@
-<?php
-	shell_exec("/usr/bin/php /home/php/yeacreate/start.php start -d");
-	
-	// $txt_file = "/tmp/_home_php_yeacreate_start.txt";
- //    if( !file_exists($txt_file) ){
-	// 	// shell_exec("/usr/bin/php /home/php/yeacreate/crontab.php > /dev/null 2>&1 ");
- //    }
-
