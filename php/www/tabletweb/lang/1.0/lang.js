d86f7828cc50e66ac185b8dd80a7475e790a291a
xuejing <xuejing@yeacreate.com>
2020-03-11 14:26:56 +0800

    20-03-11-14-25

---
+++ php/www/tabletweb/lang/1.0/lang.js
@@ -0,0 +1,254 @@
+(function (w) {
+
+
+  w.YCZH = {
+    '缩小': '缩小',
+    '本地服务连接失败': '本地服务连接失败',
+    '系统信息': '系统信息',
+    '网络设置': '网络设置',
+    '屏幕测试': '屏幕测试',
+    '电池信息': '电池信息',
+    '接口信息': '接口信息',
+    '摄像头': '摄像头',
+    '打开摄像头': '打开摄像头',
+    '关闭摄像头': '关闭摄像头',
+    '开启声音': '开启声音',
+    '静音': '静音',
+    '音量': '音量',
+    '摄像头或麦克风调用失败': '摄像头或麦克风调用失败',
+    '开': '开',
+    '关': '关',
+    'CPU温度': 'CPU温度',
+    'CPU占用': 'CPU占用',
+    '内存占用': '内存占用',
+    '储存占用': '储存占用',
+    'SD卡ID': 'SD卡ID',
+    'SD卡容量': 'SD卡容量',
+    'HDMI状态': 'HDMI状态',
+    '开机时间': '开机时间',
+    '网络状态': '网络状态',
+    '系统名称': '系统名称',
+    '系统时间': '系统时间',
+    '系统版本号': '系统版本号',
+    '网络设置': '网络设置',
+    'Mac地址': 'Mac地址',
+    '频率': '频率',
+    '信号': '信号',
+    '网络名称': '网络名称',
+    '忘记网络': '忘记网络',
+    '连接设备': '连接设备',
+    '退出': '退出',
+    '密码': '密码',
+    '请输入密码': '请输入密码',
+    '当前连接': '当前连接',
+    '重新扫描': '重新扫描',
+    '选择已保存网络': '选择已保存网络',
+    '选择网络': '选择网络',
+    '找不到网络,请重新扫描': '找不到网络,请重新扫描',
+    '网络连接失败': '网络连接失败',
+    '连接成功': '连接成功',
+    '密码有误': '密码有误',
+    '连接失败': '连接失败',
+    '操作成功': '操作成功',
+    '操作失败': '操作失败',
+    '接口信息': '接口信息',
+    'GPIO数据接口': 'GPIO数据接口',
+    '当前电压': '当前电压',
+    '获取接口信息': '获取接口信息',
+    '获取接口信息': '获取接口信息',
+    '已连接': '已连接',
+    '未连接': '未连接',
+    '屏幕亮度': '屏幕亮度',
+    '坏点测试': '坏点测试',
+    '选择测试颜色': '选择测试颜色',
+    '添加自定义颜色': '添加自定义颜色',
+    '切换模式': '切换模式',
+    '手动点击': '手动点击',
+    '自动': '自动',
+    '自动切换时间': '自动切换时间',
+    '开始测试': '开始测试',
+    '停止测试': '停止测试',
+    '未选择任何颜色': '未选择任何颜色',
+    '电池信息': '电池信息',
+    '充电中': '充电中',
+    'WebSocket网络异常': 'WebSocket网络异常',
+
+    // add
+    '已保存的WIFI连接': '已保存的WIFI连接',
+    '当前ssid密码已保存': '当前ssid密码已保存',
+    '无加密的WIFI连接': '无加密的WIFI连接',
+    '当前ssid无需密码': '当前ssid无需密码',
+    '有加密的WIFI连接': '有加密的WIFI连接',
+    '请输入WIFI密码': '请输入WIFI密码',
+    '播放声音': '播放声音',
+    '声音录制': '声音录制',
+    '停止录制': '停止录制',
+    '停止播放': '停止播放',
+    '自动轮询': '自动轮询'
+  }
+
+  w.YCEN = {
+    '已保存的WIFI连接': 'Saved WIFI connection',
+
+    '当前ssid密码已保存': 'The password of this SSID have been saved',
+
+    '无加密的WIFI连接': 'None password',
+
+    '当前ssid无需密码': 'This SSID do not need password',
+
+    '有加密的WIFI连接': 'Encrypted',
+
+    '请输入WIFI密码': 'Please input the password of WIFI',
+
+    '播放声音': 'Play',
+
+    '声音录制': 'Record',
+
+    '停止录制': 'Stop recording',
+
+    '停止播放': 'Stop',
+
+    '自动轮询': 'Auto run',
+
+    '缩小': 'Minisize',
+
+    '本地服务连接失败': 'Can not connect to the local service',
+
+    '系统信息': 'System\nInformation',
+
+    '网络设置t': 'Networking\nSetup',
+
+    '屏幕测试t': 'Screen\nTest',
+
+    '电池信息t': 'Battery\nInformation',
+
+    '接口信息t': 'GPIO Test',
+
+    '摄像头': 'Camera',
+
+    '打开摄像头': 'Enable Camera',
+
+    '关闭摄像头': 'Disable Camera',
+
+    '开启声音': 'Enable Voice',
+
+    '静音': 'Mute',
+
+    '音量': 'Volume',
+
+    '摄像头或麦克风调用失败': 'Camera or microphone call failed',
+
+    '开': 'Enable',
+
+    '关': 'Disable',
+
+    'CPU温度': 'Temperature',
+
+    'CPU占用': 'CPU',
+
+    '内存占用': 'RAM',
+
+    '储存占用': 'Flash',
+
+    'SD卡ID': 'SD ID',
+
+    'SD卡容量': 'SD Size',
+
+    'HDMI状态': 'HDMI',
+
+    '开机时间': 'Up Time',
+
+    '网络状态': 'Networking',
+
+
+    '系统名称': 'System Name',
+
+    '系统时间': 'Time',
+
+    '系统版本号': 'Version',
+
+    '网络设置': 'Networking Setup',
+
+    'Mac地址': 'Mac Address',
+
+    '频率': 'Frequency',
+
+    '信号': 'Signal',
+
+    '网络名称': 'Networking Name',
+
+    '忘记网络': 'Forget this network',
+
+    '连接设备': 'Connect',
+
+    '退出': 'Exit',
+
+    '密码': 'Password',
+
+    '请输入密码': 'Please input the password',
+
+    '当前连接': 'Connected to',
+
+    '重新扫描': 'Scan',
+
+    '选择已保存网络': 'Please select saved network',
+
+    '选择网络': 'Select Networking',
+
+    '找不到网络,请重新扫描': 'Can not find this networking, please scan it again',
+
+    '网络连接失败': 'Networking connecting failed',
+
+    '连接成功': 'Connected',
+
+    '密码有误': 'Wrong password',
+
+    '连接失败': 'Connection failed',
+
+    '操作成功': 'Success',
+
+    '操作失败': 'Failed',
+
+    '接口信息': 'Interface information',
+
+    'GPIO数据接口': 'GPIO Test',
+
+    '当前电压': 'Voltage',
+
+    '获取接口信息': 'Get the information form the interface',
+
+    '已连接': 'Connecting',
+
+    '未连接': 'Disconnected',
+
+    '屏幕亮度': 'Brightness',
+
+    '坏点测试': 'Dead pixels testing',
+
+    '选择测试颜色': 'Please select color',
+
+    '添加自定义颜色': 'Customize color',
+
+    '切换模式': 'Switch Mode',
+
+    '手动点击': 'Cick manually',
+
+    '自动': 'Automatic',
+
+    '自动切换时间': 'Switching time',
+
+    '开始测试': 'Begin',
+
+    '停止测试': 'Stop',
+
+    '未选择任何颜色': 'Please select color',
+
+    '电池信息': 'Battery Information',
+
+    '充电中': 'Charging',
+
+    'WebSocket网络异常': 'WebSocket Failed',
+  }
+
+
+})(window)
