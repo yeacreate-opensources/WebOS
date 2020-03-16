bb1275db4da9710c50c7890a4dee8faedaa33b3e
xuejing <xuejing@yeacreate.com>
2020-03-16 15:31:33 +0800

    20-03-16-15-31

--- php/www/index.php
+++ php/www/index.php
@@ -52,15 +52,16 @@ shell_exec("chmod -R 0755 {$etc_cp}S66phpwebsocketserver");
 
 $input_conf = "/etc/input-event-daemon.conf";
 $content_input = @file_get_contents($input_conf); //读文件
-$content_input = @str_replace('VOLUMEUP     = amixer -q set DAC \'2%+\'','VOLUMEUP     = /usr/bin/php /home/php/yeacreate/volume.php \'+\'',$content_input);
-$content_input = @str_replace('VOLUMEDOWN   = amixer -q set DAC \'2%-\'','VOLUMEDOWN   = /usr/bin/php /home/php/yeacreate/volume.php \'-\'',$content_input);;
+$find = @array("VOLUMEUP     = amixer -q set DAC '2%+'","VOLUMEDOWN   = amixer -q set DAC '2%-'");
+$replace = @array("VOLUMEUP     = /usr/bin/php /home/php/yeacreate/volume.php '+'","VOLUMEDOWN   = /usr/bin/php /home/php/yeacreate/volume.php '-'");
+$content_input = @str_replace($find,$replace,$content_input);;
 @file_put_contents($input_conf, $content_input);
     
 shell_exec("dos2unix {$input_conf}");
 
 $s91chromium = "{$etc_cp}S91chromium";
 $content_s91chromium = @file_get_contents($s91chromium); //读文件
-$content_s91chromium = @str_replace('http://127.0.0.1','/home/php/www/tabletweb/index.html',$content_s91chromium);
+$content_s91chromium = @str_replace(array('http://127.0.0.1','http://localhost'),'/home/php/www/tabletweb/index.html',$content_s91chromium);
 $content_s91chromium_data = @explode('chmod 666 /dev/video-enc0;',$content_s91chromium);
 $content_s91chromium = $content_s91chromium_data[0].'chmod 666 /dev/video-enc0;'."\n\t\t\t\tamixer -q set DAC '100%';".$content_s91chromium_data[1];
 @file_put_contents($s91chromium, $content_s91chromium);
