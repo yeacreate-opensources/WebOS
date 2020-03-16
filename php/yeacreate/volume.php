e25ea63682bf50646a784c71752f14bc5f9cda57
xuejing <xuejing@yeacreate.com>
2020-03-16 12:09:14 +0800

    20-03-16-12-09

--- php/yeacreate/volume.php
+++ php/yeacreate/volume.php
@@ -38,23 +38,25 @@ use WebSocket\{ConnectionException,BadOpcodeException,BadUriException};
 	$volume_size = isset($argv[1]) ? "{$argv[1]}" : '';
 	$get_current = explode('%', shell_exec("amixer get DAC | grep 'Right:' | awk -F '[][]' '{ print $2 }'"))[0];
 	if( $volume_size == '+' ){
-		$get_current_a = $get_current+1;
+		$get_current_a = $get_current+2;
 		if($get_current_a >= 100){
 			exec("amixer -q set DAC '100%'");
 		}else{
-			if($get_current_a <= 50){
-				exec("amixer -q set DAC '51%'");
+			if($get_current_a <= 61){
+				exec("amixer -q set DAC '62%'");
 			}else{
-				exec("amixer -q set DAC '1%+'");
+				$get_current_a = $get_current_a.'%';
+				exec("amixer -q set DAC '{$get_current_a}'");
 			}
 		}
 		$volume = 1;
 	}else if( $volume_size == '-' ){
-		$get_current_b = $get_current-1;
-		if( $get_current_b <= 50 ){
+		$get_current_b = $get_current-2;
+		if( $get_current_b <= 61 ){
 			exec("amixer -q set DAC '0'");
 		}else{
-			exec("amixer -q set DAC '1%-'");
+			$get_current_b = $get_current_b.'%';
+			exec("amixer -q set DAC '$get_current_b'");
 		}
 		$volume = 1;
 	}
