e9c4eb36e101da1ee8c2bd52bab2a81f4822fd67
xuejing <xuejing@yeacreate.com>
2020-06-05 09:07:00 +0800

    20-06-05

--- php/yeacreate/stop.php
+++ php/yeacreate/stop.php
@@ -29,5 +29,5 @@
 # php stop.php
 ################################################################################
  */
-$php_php = exec("which php");
+$php_php = @trim(str_replace(PHP_EOL,'',shell_exec("which php")));
 shell_exec("{$php_php} /home/php/yeacreate/gpio.php > /dev/null 2>&1 &");
