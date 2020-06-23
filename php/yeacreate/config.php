de184d5792f206660d61606b334718f8142a1779
xuejing <xuejing@yeacreate.com>
2020-06-23 09:46:48 +0800

    20-06-23

--- php/yeacreate/config.php
+++ php/yeacreate/config.php
@@ -30,7 +30,7 @@
  */
 $SIGNALING_ADDRESS_WEBSOCKET = 'websocket://localhost:2020';
 $SIGNALING_ADDRESS_WS = 'ws://localhost:2020';
-$system_ver_arr = '1.7';
+$system_ver_arr = '1.8';
 $system_name_arr = 'Yea Create Webos';
 
 
